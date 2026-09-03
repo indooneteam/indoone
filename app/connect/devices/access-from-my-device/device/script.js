d4463efca490f2e7a82143a76d16b274e142a538
Indomark <indomark@zohomail.in>
2026-09-03 16:20:39 +0530

    Add access-from-my-device detail logic

---
+++ app/connect/devices/access-from-my-device/device/script.js
@@ -0,0 +1,8 @@
+(() => {
+  window.showAccessFromMyDevice = async () => {
+    const content = document.getElementById('deviceContent');
+    if (!content) return;
+
+    content.innerHTML = '<div>Device detail is ready.</div>';
+  };
+})();
