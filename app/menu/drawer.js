5e98d143dda7eae9a0d58344a889de80fd0dbdcd
Indomark <indomark@zohomail.in>
2026-09-03 09:34:09 +0530

    Remove obsolete Menu drawer runtime

--- app/menu/drawer.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const src = 'app/menu/script.js?v=20260903b';
-  if ([...document.scripts].some(script => script.src.includes('/app/menu/script.js'))) return;
-  const script = document.createElement('script');
-  script.src = src;
-  document.body.appendChild(script);
-})();
