e26ea2e9b94f9737e2c8fc936f0cabfe62e0d103
Indomark <indomark@zohomail.in>
2026-09-05 10:50:28 +0530

    Format lock menu script for readability

--- app/menu/lock/script.js
+++ app/menu/lock/script.js
@@ -1,7 +1,12 @@
 window.initMenuLock = function () {
   closeDrawer();
   closeModal();
-  if (typeof lockIndoone === 'function') lockIndoone();
-  else toast('Lock is unavailable.');
+
+  if (typeof lockIndoone === 'function') {
+    lockIndoone();
+  } else {
+    toast('Lock is unavailable.');
+  }
+
   return false;
 };
