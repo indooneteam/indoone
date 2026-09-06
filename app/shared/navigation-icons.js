7067fac59c3a90e97be9bca47d0e590477c46d3f
Indomark <indomark@zohomail.in>
2026-09-06 09:07:52 +0530

    Bump shared branding loader to new logo version

--- app/shared/navigation-icons.js
+++ app/shared/navigation-icons.js
@@ -2,7 +2,7 @@
 (() => {
   const brandingLoader = document.createElement('script');
   brandingLoader.src =
-    'app/shared/branding.js?v=20260906-logo-1';
+    'app/shared/branding.js?v=20260906-logo-3';
   brandingLoader.async = false;
   document.head.appendChild(brandingLoader);
 
