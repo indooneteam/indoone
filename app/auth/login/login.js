6e31163cae55300b89a47be969f26d9d48d3eab3
Indomark <indomark@zohomail.in>
2026-09-06 08:53:11 +0530

    Refresh login logo cache version

--- app/auth/login/login.js
+++ app/auth/login/login.js
@@ -48,7 +48,7 @@
       <div class="auth-brand">
         <img
           class="auth-mark"
-          src="assets/branding/indoone-master.svg?v=20260906-logo-1"
+          src="assets/branding/indoone-master.svg?v=20260906-logo-2"
           alt="Indoone logo"
         />
         <div>
