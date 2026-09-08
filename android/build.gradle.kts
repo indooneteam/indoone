4977ebe68a41678076b6384abaff16b6dad29a34
Indomark <indomark@zohomail.in>
2026-09-08 18:27:03 +0530

    feat(android): add biometric authentication dependency

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-database")
+    implementation("androidx.biometric:biometric:1.1.0")
 }
