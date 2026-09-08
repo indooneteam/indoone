66c1a677987c830fb7f8a2e7cdb61551443c616d
Indomark <indomark@zohomail.in>
2026-09-08 16:52:51 +0530

    Add Android Accounts view model

---
+++ android/accounts/AccountsViewModel.kt
@@ -0,0 +1,41 @@
+package com.indoone.accounts
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AccountsViewModel : ViewModel() {
+    private val _state = MutableStateFlow(AccountsState())
+    val state: StateFlow<AccountsState> = _state.asStateFlow()
+
+    fun setAccounts(accounts: List<AccountItem>) {
+        _state.value = _state.value.copy(accounts = accounts)
+    }
+
+    fun updateSearchQuery(query: String) {
+        _state.value = _state.value.copy(searchQuery = query)
+    }
+
+    fun clearSearch() {
+        _state.value = _state.value.copy(searchQuery = "")
+    }
+
+    fun toggleSort() {
+        _state.value = _state.value.copy(
+            sortAscending = !_state.value.sortAscending,
+        )
+    }
+
+    fun toggleFavorite(accountId: String) {
+        _state.value = _state.value.copy(
+            accounts = _state.value.accounts.map { account ->
+                if (account.id == accountId) {
+                    account.copy(favorite = !account.favorite)
+                } else {
+                    account
+                }
+            },
+        )
+    }
+}
