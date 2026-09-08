c6d2acdff73dd1d60a5d187de69d24b111e8a644
Indomark <indomark@zohomail.in>
2026-09-08 20:06:00 +0530

    Move app unlock activity into settings/unlock

---
+++ android/settings/unlock/UnlockAppActivity.kt
@@ -0,0 +1,57 @@
+package com.indoone.settings.unlock
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.indoone.settings.applock.AppLockStore
+
+class UnlockAppActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val store = AppLockStore(applicationContext)
+        if (!store.isEnabled()) {
+            finish()
+            return
+        }
+
+        setContent {
+            MaterialTheme {
+                var pin by remember { mutableStateOf("") }
+                var error by remember { mutableStateOf("") }
+
+                UnlockAppScreen(
+                    pin = pin,
+                    error = error,
+                    onDigit = { digit ->
+                        if (pin.length < 12) {
+                            pin += digit
+                            error = ""
+                        }
+                    },
+                    onBackspace = {
+                        pin = pin.dropLast(1)
+                        error = ""
+                    },
+                    onClear = {
+                        pin = ""
+                        error = ""
+                    },
+                    onUnlock = {
+                        if (store.verifyPin(pin)) {
+                            finish()
+                        } else {
+                            pin = ""
+                            error = "Incorrect App PIN"
+                        }
+                    },
+                )
+            }
+        }
+    }
+}
