5318c802acb28d37a9f42b65ecc3dbb7438b0069
Indomark <indomark@zohomail.in>
2026-09-08 21:11:52 +0530

    Remove duplicate Firebase exception extension

--- android/menu/danger-zone/FirebaseExceptionExtensions.kt
+++
@@ -1,6 +0,0 @@
-package com.indoone.menu.dangerzone
-
-import com.google.firebase.auth.FirebaseAuthException
-
-val Exception.code: String
-    get() = (this as? FirebaseAuthException)?.errorCode.orEmpty()
