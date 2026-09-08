d25c91f4ec76155b7133210f726f1f8bf80e19b6
Indomark <indomark@zohomail.in>
2026-09-08 20:55:18 +0530

    Fix bottom navigation weight scope

--- android/menu/AppPageChrome.kt
+++ android/menu/AppPageChrome.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -112,7 +113,7 @@ fun AppBottomNav(
 enum class AppTab { ACCOUNTS, LOBBY, CONNECT, SETTINGS }
 
 @Composable
-private fun AppBottomNavItem(
+private fun RowScope.AppBottomNavItem(
     icon: String,
     label: String,
     active: Boolean,
