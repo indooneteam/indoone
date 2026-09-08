7006ec8ca0d7854f1f3837899ad78877ddd89bb3
Indomark <indomark@zohomail.in>
2026-09-08 19:16:28 +0530

    Move deleted accounts into Android Trash

--- android/accounts/accounts/AccountRemovalService.kt
+++ android/accounts/accounts/AccountRemovalService.kt
@@ -3,13 +3,13 @@ package com.indoone.accounts.accounts
 import com.indoone.accounts.AccountRepository
 
 /**
- * Removes an authenticator account from the active account collection.
+ * Moves an authenticator account from the active collection to Trash.
  */
 class AccountRemovalService(
     private val repository: AccountRepository,
 ) {
     suspend fun remove(accountId: String): Result<Unit> = runCatching {
         require(accountId.isNotBlank()) { "Account id is required." }
-        repository.remove(accountId)
+        repository.moveToTrash(accountId)
     }
 }
