b93945079672efe5efab70ee754eaf989bfac6c2
Indomark <indomark@zohomail.in>
2026-09-09 11:42:54 +0530

    Fix Android status bar content inset

--- android/IndooneApplication.kt
+++ android/IndooneApplication.kt
@@ -3,7 +3,10 @@ package com.indoone
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.indoone.settings.autolock.AutoLockController
 
 class IndooneApplication : Application() {
@@ -15,11 +18,13 @@ class IndooneApplication : Application() {
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
                 if (activity is ComponentActivity) {
                     controllers[activity] = AutoLockController(activity.applicationContext)
+                    installStatusBarInset(activity)
                 }
             }
 
             override fun onActivityResumed(activity: Activity) {
                 controllers[activity]?.onResumed(activity as? ComponentActivity ?: return)
+                if (activity is ComponentActivity) installStatusBarInset(activity)
             }
 
             override fun onActivityPaused(activity: Activity) {
@@ -34,5 +39,18 @@ class IndooneApplication : Application() {
             override fun onActivityStopped(activity: Activity) = Unit
             override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
         })
+    }
+
+    private fun installStatusBarInset(activity: ComponentActivity) {
+        val content = activity.findViewById<View>(android.R.id.content) ?: return
+        val initialLeft = content.paddingLeft
+        val initialRight = content.paddingRight
+        val initialBottom = content.paddingBottom
+        ViewCompat.setOnApplyWindowInsetsListener(content) { view, insets ->
+            val topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            view.setPadding(initialLeft, topInset, initialRight, initialBottom)
+            insets
+        }
+        ViewCompat.requestApplyInsets(content)
     }
 }
