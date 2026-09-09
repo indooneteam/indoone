14c53f3af90f520fd1b7728163d8d6bbb741c454
Indomark <indomark@zohomail.in>
2026-09-09 19:39:54 +0530

    Fix favorites backdrop layout

--- android/menu/favorites/FavoritesScreen.kt
+++ android/menu/favorites/FavoritesScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -154,12 +155,6 @@ private fun FavoritesAccountsBackdrop(
                 onSearchClick = {},
             )
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 18.dp, end = 18.dp, top = 12.dp, bottom = 4.dp)
-                    .size(height = 44.dp, width = 1.dp),
-            )
             Surface(
                 modifier = Modifier
                     .fillMaxWidth()
