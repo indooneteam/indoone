db33aa89baef033a8952c4cd842b37596fc84d03
Indomark <indomark@zohomail.in>
2026-09-12 15:28:09 +0530

    Remove Home chat history button

--- android/home/HomeScreen.kt
+++ android/home/HomeScreen.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -188,8 +188,6 @@ fun HomeScreen(
         Column(Modifier.fillMaxSize()) {
             AppTopBar(
                 onMenuClick = onMenuClick,
-                trailingIcon = "☷",
-                onTrailingClick = ::onHistoryClick,
             )
 
             Column(
